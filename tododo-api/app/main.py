f0210db631def9cc4aeed0cebfd81f2973bc337e
roy0629 <yuya19920721@outlook.jp>
2023-06-09 01:49:03 +0900

    不要コード削除

--- tododo-api/app/main.py
+++ tododo-api/app/main.py
@@ -18,16 +18,6 @@ def root():
 
 @app.get("/db-access")
 def db_access_check():
-    # conn = psycopg2.connect(DATABASE_URL)
-    # cur = conn.cursor(cursor_factory=DictCursor)
-    # cur.execute("select * from pg_settings")
-    # db_setting_info = cur.fetchall()
-    # dict_result = []
-    # for row in db_setting_info:
-    #     dict_result.append(dict(row))
-    # cur.close()
-    # conn.close()
-    # return dict_result
     with DB() as db:
         db.cursor.execute("select * from pg_settings")
         db_setting_info = db.cursor.fetchall()
